4dfd1c4c3bcc2ab790ed3267f2dc54cf00128124
Totsapon Menkul <totsapon.m@ku.th>
2018-05-03 23:38:30 +0700

    add controller in online game ui

---
+++ src/application/WinControllerUI.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class WinControllerUI {
+	@FXML
+	private Label playerWin_label;
+	@FXML
+	private Button replay_button, restart_button, newGame_button;
+	
+	private boolean isReplay;
+	
+	public WinControllerUI() {
+		isReplay = false;
+	}
+	
+	public void  
+}
